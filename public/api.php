9f9f7961bb0c50982582150dc0cd3d7327e0617d
root <root@instance-1.us-central1-a.c.database-321815.internal>
2021-08-04 21:00:45 +0000

    updated

--- public/api.php
+++ public/api.php
@@ -4,16 +4,21 @@ $con = mysqli_connect("localhost", "root", "12345", "testdb");
 $response = array();
 
 if($con) {
-    echo "DB CONNECTED";
     $sql = "SELECT * FROM summer";
     $result = mysqli_query($con, $sql);
 
     if ($result) {
-	$i=0;
+	header("Content-Type: application/json");
+	$i=1;
         while($row = mysqli_fetch_assoc($result)) {
             $response[$i]['Year'] = $row ['Year'];
             $response[$i]['City'] = $row ['City'];
+            $response[$i]['Discipline'] = $row ['Discipline'];
             $response[$i]['Athlete'] = $row ['Athlete'];
+            $response[$i]['Country'] = $row ['Country'];
+            $response[$i]['Gender'] = $row ['Gender'];
+            $response[$i]['Event'] = $row ['Event'];
+            $response[$i]['Medal'] = $row ['Medal'];
             $i++;
         }
 
