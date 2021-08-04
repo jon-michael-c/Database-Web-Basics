573d0bfb45edeb8fbf048dd83a30967208a8dfb4
jon-michael-c <jonmichaelcubano@gmail.com>
2021-08-04 15:20:22 -0400

    updated

--- public/api.php
+++ public/api.php
@@ -1,32 +1,11 @@
 <?php
 
-require_once __DIR__ . '/config.php';
+$con = mysqli_connect("localhost", "root", "", "API_DATA");
 
-class API {
-    function Select() {
-        $db = new Connect;
-        $summer = array();
-        $data = $db->prepare('SELECT * FROM summer');
-        $data->execute();
-
-        while($OutputData = $data->fetch(PDO::FETCH_ASSOC)) {
-            $summer[$OutputData['Year']] = array(
-                'Year' => $OutputData['Year'],
-                'City' => $OutputData['City'],
-                'Sport' => $OutputData['Sport']
-            );
-        }
-
-        return json_encode($summer);
-    }
-
-
-
+if($con) {
+    echo "DB CONNECTED";
 }
 
-    $API = new API;
 
-    header('Content-Type: application/json');
 
-    echo $API->Select();
 ?>
