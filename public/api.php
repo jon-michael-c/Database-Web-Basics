5e46826d2000374ba90bd386fd1cbc77b2c31d2d
jon-michael-c <jonmichaelcubano@gmail.com>
2021-08-04 15:35:40 -0400

    updated

--- public/api.php
+++ public/api.php
@@ -1,10 +1,28 @@
 <?php
 
-$con = mysqli_connect("localhost", "root", "", "API_DATA");
+$con = mysqli_connect("localhost", "root", "12345", "testdb");
+$response = array();
 
 if($con) {
     echo "DB CONNECTED";
+    $sql = "SELECT * from summer";
+    $result = mysqli_query($con, $sql);
+
+    if ($result) {
+        while($row = mysqli_fetch_assoc($result)) {
+            $response[$i]['Year'] = $row ['Year'];
+            $response[$i]['City'] = $row ['City'];
+            $response[$i]['Athlete'] = $row ['Athlete'];
+            $i++;
+        }
+
+        echo json_encode($response, JSON_PRETTY_PRINT);
+    }
+} else {
+    echo "DB CONNECTION FALIED"
 }
+
+
 
 
 
