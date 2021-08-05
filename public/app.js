07b1de61bee64456e5c6c972781024f3f68d7606
jon-michael-c <jonmichaelcubano@gmail.com>
2021-08-05 11:31:14 -0400

    updated

--- public/app.js
+++ public/app.js
@@ -1,22 +1,35 @@
 const summerList = document.getElementById('summer-list');
 const searchBar = document.getElementById('searchBar');
-let athletes = [];
+const status = document.getElementById('status');
+let summerdata = [];
 
+// Search Bar
+searchBar.addEventListener('keyup', (e) => {
+    const target = e.target.value;
+    const filteredAthletes = summerdata.filter( (athlete) => {
+        return athlete.Athlete.includes(target) || athlete.country.includes(target);
+    });
+
+    console.log(filteredAthletes);
+});
 
 // Grab API
 const loadSummer = async () => {
     try {
         const res = await fetch('http://34.121.233.142/Database-Web-Basics/public/api.php');
         summerdata = await res.json();
-        displayData(athletes);
+        status.innerHTML = "API CONNECTED";
+        console.log(summerdata);
+        displayData(summerdata);
     } catch (err) {
+        status.innerHTML = "API NOT CONNECTED";
         console.error(err);
     }
 };
 
 // Display Results
-const displayData = (data) => {
-    const htmlString = data
+const displayData = (athletes) => {
+    const htmlString = athletes
         .map((athlete) => {
             return `
             <li class="entry">
@@ -31,12 +44,3 @@ const displayData = (data) => {
 
 loadSummer();
 
-// Search Bar
-searchBar.addEventListener('keyup', (e) => {
-    const target = e.target.value;
-    const filteredAthletes = athletes.filter( (athlete) => {
-        return athlete.includes(target) || athlete.country.includes(target);
-    });
-
-    console.log(filteredAthletes);
-});
